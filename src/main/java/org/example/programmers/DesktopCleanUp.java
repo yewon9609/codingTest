40c57297244258c574ef91e1fe672bae917f8ee5
yewon.seo <dodnjs1241@naver.com>
2023-03-24 15:11:05 +0900

    프로그래머스 - 바탕화면 정리

---
+++ src/main/java/org/example/programmers/DesktopCleanUp.java
@@ -0,0 +1,30 @@
+package org.example.programmers;
+
+import java.util.Arrays;
+
+public class DesktopCleanUp {
+  public int[] solution(String[] wallpaper) {
+    int lux = Integer.MAX_VALUE, luy = Integer.MAX_VALUE, rdx = 0, rdy = 0;
+
+    for (int i = 0; i < wallpaper.length; i++) {
+      for (int j = 0; j < wallpaper[i].length(); j++) {
+        if(wallpaper[i].charAt(j) == '#') {
+          lux = Math.min(lux, i);
+          luy = Math.min(luy, j);
+          rdx = Math.max(rdx, i);
+          rdy = Math.max(rdy, j);
+        }
+      }
+    }
+
+    return new int[]{lux, luy, rdx+1, rdy+1};
+  }
+
+  public static void main(String[] args) {
+    DesktopCleanUp desktop = new DesktopCleanUp();
+    String[] wallpaper = {"..........", ".....#....", "......##..", "...##.....", "....#....."};
+
+    System.out.println(Arrays.toString(desktop.solution(wallpaper)));
+  }
+
+}

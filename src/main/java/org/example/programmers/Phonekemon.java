ac786f130c7899f0a46e50f9ae53f6c7bbd0a0bb
yewon.seo <dodnjs1241@naver.com>
2023-03-31 12:16:56 +0900

    프로그래머스 - 폰켓몬

---
+++ src/main/java/org/example/programmers/Phonekemon.java
@@ -0,0 +1,27 @@
+package org.example.programmers;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Phonekemon {
+  public int solution(int[] nums) {
+    int denominator = 2;
+    int expectResult = nums.length / denominator;
+    Set<Integer> distinctNums = new HashSet<>(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+
+    if(distinctNums.size() < expectResult) {
+      return distinctNums.size();
+    }
+
+    return expectResult;
+  }
+
+  public static void main(String[] args) {
+    Phonekemon phonekemon = new Phonekemon();
+    int[] nums = {3,3,3,2,2,2};
+    int answer = phonekemon.solution(nums);
+    System.out.println(answer);
+  }
+}

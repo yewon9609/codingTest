044c22fce1296bf9bb77581ae83b43474ecb7cbd
yewon.seo <dodnjs1241@naver.com>
2023-03-31 12:30:18 +0900

    프로그래머스 - 덧칠하기

--- src/main/java/org/example/programmers/Lv1/Overpainting.java
+++ src/main/java/org/example/programmers/Lv1/Overpainting.java
@@ -15,16 +15,4 @@ public class Overpainting {
     
     return answer;
   }
-
-  public static void main(String[] args) {
-    Overpainting overpainting = new Overpainting();
-    //구역
-    int n = 8;
-    //롤러 길이
-    int m = 4;
-    //덧칠해야 하는 부분
-    int[] section = {2,3,6};
-    System.out.println(overpainting.solution(n, m, section));
-  }
-
 }

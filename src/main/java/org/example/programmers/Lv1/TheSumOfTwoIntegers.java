0855653999b40ae80b7eef98509a82a2aafe5d7a
yewon.seo <dodnjs1241@naver.com>
2023-04-13 22:49:31 +0900

    프로그래머스 - 두 정수 사이의 합

---
+++ src/main/java/org/example/programmers/Lv1/TheSumOfTwoIntegers.java
@@ -0,0 +1,20 @@
+package org.example.programmers.Lv1;
+
+public class TheSumOfTwoIntegers {
+
+  public long solution(int a, int b) {
+
+    long answer = 0;
+
+    if(a==b) {
+      return a;
+    }
+
+    for (int i = Math.min(a, b); i <= Math.max(a, b); i++) {
+      answer += i;
+    }
+
+    return answer;
+  }
+
+}

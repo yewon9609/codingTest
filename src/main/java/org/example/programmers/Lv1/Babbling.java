d3e548e7758bfb04bd3fa7f96e27e0bf6244a43d
yewon.seo <dodnjs1241@naver.com>
2023-07-20 23:44:49 +0900

    프로그래머스 - 옹알이(2)

--- src/main/java/org/example/programmers/Lv1/Babbling.java
+++ src/main/java/org/example/programmers/Lv1/Babbling.java
@@ -1,12 +1,33 @@
 package org.example.programmers.Lv1;
 
-import java.util.List;
-
 public class Babbling {
+
   public int solution(String[] babbling) {
+    int answer = 0;
+    String aya = "aya", ye = "ye", woo = "woo", ma = "ma";
 
+    for (int i = 0; i < babbling.length; i++) {
+
+      if(babbling[i].contains(aya.concat(aya))
+        || babbling[i].contains(ye.concat(ye))
+        || babbling[i].contains(woo.concat(woo))
+        || babbling[i].contains(ma.concat(ma))) {
+        continue;
+      }
+
+      String replaceBabbling = babbling[i].replace(aya, "")
+          .replace(ye, "")
+          .replace(woo, "")
+          .replace(ma, "");
+
+      if (replaceBabbling.isEmpty()) {
+        answer++;
+      }
+
+    }
+
+    return answer;
   }
-
 
   public static void main(String[] args) {
     Babbling b = new Babbling();

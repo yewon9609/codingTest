73381f065a1ae92fa10bc94bf648828327af43f0
yewon.seo <dodnjs1241@naver.com>
2023-07-20 23:31:20 +0900

    프로그래머스 - 옹알이(2)

---
+++ src/main/java/org/example/programmers/Lv1/Babbling.java
@@ -0,0 +1,17 @@
+package org.example.programmers.Lv1;
+
+import java.util.List;
+
+public class Babbling {
+  public int solution(String[] babbling) {
+
+  }
+
+
+  public static void main(String[] args) {
+    Babbling b = new Babbling();
+    String[] babbling = {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
+    int solution = b.solution(babbling);
+    System.out.println(solution);
+  }
+}

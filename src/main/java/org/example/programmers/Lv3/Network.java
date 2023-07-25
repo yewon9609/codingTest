c26dab21ae576262b3f62af9978c0cacc8122d1a
yewon.seo <dodnjs1241@naver.com>
2023-07-25 22:23:02 +0900

    프로그래머스 - 네트워크

---
+++ src/main/java/org/example/programmers/Lv3/Network.java
@@ -0,0 +1,37 @@
+package org.example.programmers.Lv3;
+
+public class Network {
+
+  public int solution(int n, int[][] computers) {
+    int answer = 0;
+    boolean[] check = new boolean[n];
+
+    for (int i = 0; i < n; i++) {
+      if (!check[i]) {
+        dfs(computers, i, check);
+        answer++;
+      }
+
+    }
+
+    return answer;
+  }
+
+  boolean[] dfs(int[][] computers, int i, boolean[] check) {
+    check[i] = true;
+
+    for (int j = 0; j < computers.length; j++) {
+      if (i != j && computers[i][j] == 1 && check[j] == false) {
+        check = dfs(computers, j, check);
+      }
+    }
+    return check;
+  }
+
+  public static void main(String[] args) {
+    Network n = new Network();
+    int[][] coumputers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+    int solution = n.solution(3, coumputers);
+  }
+
+}
